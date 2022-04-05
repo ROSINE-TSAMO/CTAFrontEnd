0a3de08125812c51d4e05c586e7c9b59769d6197
Rosine <r.tsamo@lunaflpartner.it>
2022-04-05 15:46:47 +0200

    Fix the image of pack

--- src/app/smart-contract/smart-contract.component.ts
+++ src/app/smart-contract/smart-contract.component.ts
@@ -18,7 +18,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 
 export class SmartContractComponent implements OnInit {
   encryptMsg: any;
-  images = ['../../assets/images/booster-rouge.gif', '../../assets/images/booster-bleu.gif', '../../assets/images/booster-vert.gif'];
+  images = ['../../assets/images/booster-rouge.gif', '../../assets/images/booster-vert.gif', '../../assets/images/booster-bleu.gif'];
   wallet: any;
   signer: any;
   addressesContract = "0x591D63bd26b370BB2C67a52c942D024a08E2fcD5";
@@ -34,12 +34,16 @@ export class SmartContractComponent implements OnInit {
     ["2", "500"]
   ])
 
-  constructor(private winref: WinRefService, private modalService: NgbModal, private spinnerService: NgxSpinnerService, private lambdaApi:LambdaApiService) {
+  constructor(private winref: WinRefService, private modalService: NgbModal, private spinnerService: NgxSpinnerService, private lambdaApi: LambdaApiService) {
   }
 
   async ngOnInit() {
+
+
+
+
     this.wallet = this.winref.window.ethereum;
-    
+
     //this.connectPolygon()
     //if variable localStorage is null, call the modal windows 
     if (localStorage.getItem('terminiCondizioni') == null) {
@@ -72,63 +76,70 @@ export class SmartContractComponent implements OnInit {
 
   //Communication between FE and samrt contract
   async smartContract(typeOfCard: any) {
-    if (this.wallet) {
-      const provider = new ethers.providers.Web3Provider(this.wallet);
-      this.signer = provider.getSigner();
-      let chainId = await this.signer.getChainId();
-      //console.log("chainId", chainId)
-      if (chainId !== 80001   /*137*/) {
-        //this.alertWhiteList("Please change your network to polygon");
-        this.alertWhiteList("Please change your network to mumbai");
-      }
-      else {
-        let userAddress = await this.signer.getAddress()
-        //check if is the date for minting 
-        let mintingDay = this.checkTime();
-        if (mintingDay) {
-          try {
-            //Set the smart contract
-            this.ctaContract = new ethers.Contract(this.addressesContract, StandardPack.abi, this.signer);
-            //Call the function of smart contract
-            this.mintNft = (await this.ctaContract.create(Number(typeOfCard), userAddress, { value: ethers.utils.parseEther('0.0002'), }));
-            //this.mintNft = (await this.ctaContract.create(Number(typeOfCard), userAddress, { value: ethers.utils.parseEther(this.promotionCard.get(typeOfCard)!), }));
-            this.spinner = true;
-            this.showSpinner();
-            //Wait execution of minting token
-            let tx = await this.mintNft.wait();
-            let event = tx.events[0];
-
-            let transactionHash = event.transactionHash;
-            this.spinner = false
-            this.hideSpinner()
-            let url = "https://mumbai.polygonscan.com/tx/" + transactionHash;
-
-            Swal.fire({
-              title: 'NFT minted!',
-              html: '<a target="bank" href="' + url + '">Click here to see details of NFT here</a>',
-              confirmButtonColor: '#02031f',
-              heightAuto: false,
-            })
-          } catch (err: any) {
-            this.alertWhiteList(err["data"]["message"]);
-          }
+    try {
+      if (this.wallet) {
+        const provider = new ethers.providers.Web3Provider(this.wallet);
+        this.signer = provider.getSigner();
+        let chainId = await this.signer.getChainId();
+        //console.log("chainId", chainId)
+        if (chainId !== 80001   /*137*/) {
+          //this.alertWhiteList("Please change your network to polygon");
+          this.alertWhiteList("Please change your network to mumbai");
         }
         else {
-          this.alertWhiteList("You can't mint this package because it's not a day of minting");
+          let userAddress = await this.signer.getAddress()
+          //check if is the date for minting 
+          let mintingDay = this.checkTime();
+          if (mintingDay) {
+            try {
+              //Set the smart contract
+              this.ctaContract = new ethers.Contract(this.addressesContract, StandardPack.abi, this.signer);
+              //Call the function of smart contract
+              //this.mintNft = (await this.ctaContract.create(Number(typeOfCard), userAddress, { value: ethers.utils.parseEther('0.0001'), }));
+              this.mintNft = (await this.ctaContract.create(Number(typeOfCard), userAddress, { value: ethers.utils.parseEther(this.promotionCard.get(typeOfCard)!), }));
+              this.spinner = true;
+              this.showSpinner();
+              //Wait execution of minting token
+              let tx = await this.mintNft.wait();
+              let event = tx.events[0];
+
+              let transactionHash = event.transactionHash;
+              this.spinner = false
+              this.hideSpinner()
+              let url = "https://mumbai.polygonscan.com/tx/" + transactionHash;
+
+              Swal.fire({
+                title: 'NFT minted!',
+                html: '<a target="bank" href="' + url + '">Click here to see details of NFT here</a>',
+                confirmButtonColor: '#02031f',
+                heightAuto: false,
+              })
+            } catch (err: any) {
+              this.alertWhiteList(err["data"]["message"]);
+            }
+          }
+          else {
+            this.alertWhiteList("You can't mint this package because it's not a day of minting");
+          }
         }
       }
+      else {
+        this.alertWhiteList("Please Install metamask");
+      }
+
     }
-    else {
-      this.alertWhiteList("Please Install metamask");
+    catch (error) {
+      this.alertWhiteList("Please connect the site to metamask");
+
     }
   }
   //call the the api to send message
   sendMessages() {
     this.lambdaApi.getMessage()
-    .subscribe(data => 
-      console.log("Msg from api:",data))
+      .subscribe(data =>
+        console.log("Msg from api:", data))
   }
-  
+
   alertWhiteList(msg: any) {
     Swal.fire({
       title: "<i class='fas fa-exclamation-triangle'></i> ops...",
