3a0894ea167ef42683bf19e138c3ac6f505d6ae3
Francesco Cerio <francescocerio@gmail.com>
2022-04-11 09:43:08 +0200

    update sale closed banner

--- src/app/smart-contract/smart-contract.component.ts
+++ src/app/smart-contract/smart-contract.component.ts
@@ -46,12 +46,12 @@ export class SmartContractComponent implements OnInit {
   }
 
   async ngOnInit() {
-    //this.showAlertClosedSale('Next sale will open soon. Please check social media for announcement!')
+    this.showAlertClosedSale('Next sale will open soon. Please check our social media for announcement!')
 
-    this.wallet = this.winref.window.ethereum;
+    // this.wallet = this.winref.window.ethereum;
     //this.connectPolygon()
-    this.connectMumbai();
-    this.connectWallet();
+    // this.connectMumbai();
+    // this.connectWallet();
     //if variable localStorage is null, call the modal windows 
     if (localStorage.getItem('terminiCondizioni') == null) {
       this.openModal();
@@ -95,7 +95,7 @@ export class SmartContractComponent implements OnInit {
         this.signer = provider.getSigner();
         let chainId = await this.signer.getChainId();
         //console.log("chainId", chainId)
-        if (chainId !== 80001    /*80001*/) {
+        if (chainId !== 137    /*80001*/) {
           //this.alertWhiteList("Please change your network to polygon");
           this.alertWhiteList("Please change your network to mumbai");
         }
@@ -216,7 +216,7 @@ export class SmartContractComponent implements OnInit {
 
   showAlertClosedSale(msg: any) {
     Swal.fire({
-      title: "<i  class='fa-light fa-face-pensive'></i> Sale is Closed",
+      title: "<i  class='fa-light fa-face-pensive'></i> SALE IS CLOSED",
       text: msg,
       heightAuto: false,
       showConfirmButton: false,
